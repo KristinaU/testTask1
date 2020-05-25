fc801798689538bd67a015edb21f3c8a71f5c4ec
KristinaU <urosova.k@gmail.com>
2020-05-25 14:47:02 +0100

    add first test (to fail)

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+class UserTest(APITestCase):
+    def test_registration(self):
+        url = reverse('registration')
+        data = {'TestUser'}
+        response = self.client.post(url, data, format='json')
+        print(response)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
