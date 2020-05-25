780976ae6953c624257473f11ea65eb492c64f16
KristinaU <urosova.k@gmail.com>
2020-05-25 15:15:27 +0100

    Add User model

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+from rest_framework import status, request
+from rest_framework.response import Response
+
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=32, primary_key=True)
+
+    USERNAME_FIELD = 'username'
+
+    def create(self, username):
+        if not username:
+            raise ValueError('Username is required')
+        return self
+
+    def get_user_model(self):
+        return self
